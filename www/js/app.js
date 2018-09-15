d7e10562bf3fc447078f1c929ae80b84984df984
Grapevine <jinsw2001@gmail.com>
2018-09-15 17:59:28 +0900

    init

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,7 @@
 // Dom7
 var $$ = Dom7;
 
+
 // Framework7 App main instance
 var app  = new Framework7({
   root: '#app', // App root element
@@ -31,6 +32,8 @@ var mainView = app.views.create('.view-main', {
   url: '/'
 });
 
+
+
 // Login Screen Demo
 $$('#my-login-screen .login-button').on('click', function () {
   var username = $$('#my-login-screen [name="username"]').val();
@@ -42,3 +45,147 @@ $$('#my-login-screen .login-button').on('click', function () {
   // Alert username and password
   app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
 });
+
+
+// Init Messages
+var messages = app.messages.create({
+  el: '.messages',
+
+  // First message rule
+  firstMessageRule: function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+    /* if:
+      - there is no previous message
+      - or previous message type (send/received) is different
+      - or previous message sender name is different
+    */
+    if (!previousMessage || previousMessage.type !== message.type || previousMessage.name !== message.name) return true;
+    return false;
+  },
+  // Last message rule
+  lastMessageRule: function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+    /* if:
+      - there is no next message
+      - or next message type (send/received) is different
+      - or next message sender name is different
+    */
+    if (!nextMessage || nextMessage.type !== message.type || nextMessage.name !== message.name) return true;
+    return false;
+  },
+  // Last message rule
+  tailMessageRule: function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+    /* if (bascially same as lastMessageRule):
+    - there is no next message
+    - or next message type (send/received) is different
+    - or next message sender name is different
+  */
+    if (!nextMessage || nextMessage.type !== message.type || nextMessage.name !== message.name) return true;
+    return false;
+  }
+});
+
+// Init Messagebar
+var messagebar = app.messagebar.create({
+  el: '.messagebar'
+});
+
+// Response flag
+var responseInProgress = false;
+
+// Send Message
+$$('.send-link').on('click', function () {
+
+  var text = messagebar.getValue().replace(/\n/g, '<br>').trim();
+  // return if empty message
+  if (!text.length) return;
+
+  // Clear area
+  messagebar.clear();
+
+  // Return focus to area
+  messagebar.focus();
+
+  // Add message to messages
+  messages.addMessage({
+    text: text,
+  });
+
+  if (responseInProgress) return;
+  // Receive dummy message
+  receiveMessage();
+});
+
+
+// Dummy response
+var answers = [
+  'Yes!',
+  'No',
+  'Hm...',
+  'I am not sure',
+  'And what about you?',
+  'May be ;)',
+  'Lorem ipsum dolor sit amet, consectetur',
+  'What?',
+  'Are you sure?',
+  'Of course',
+  'Need to think about it',
+  'Amazing!!!'
+]
+var people = [
+  {
+    name: 'Kate Johnson',
+    avatar: 'http://lorempixel.com/100/100/people/9'
+  },
+  {
+    name: 'Blue Ninja',
+    avatar: 'http://lorempixel.com/100/100/people/7'
+  }
+];
+function receiveMessage() {
+  responseInProgress = true;
+  setTimeout(function () {
+    // Get random answer and random person
+    var answer = answers[Math.floor(Math.random() * answers.length)];
+    var person = people[Math.floor(Math.random() * people.length)];
+
+    // Show typing indicator
+    messages.showTyping({
+      header: person.name + ' is typing',
+      avatar: person.avatar
+    });
+
+    setTimeout(function () {
+      // Add received dummy message
+      messages.addMessage({
+        text: answer,
+        type: 'received',
+        name: person.name,
+        avatar: person.avatar
+      });
+      // Hide typing indicator
+      messages.hideTyping();
+      responseInProgress = false;
+    }, 4000);
+  }, 1000);
+}
+
+
+axios.get('https://grapevine-chatserver.mybluemix.net/listPhoto')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
